d0d70808a86374a426336a285210736d8cf6f24d
Gabriel Medina <gabi89.m@gmail.com>
2018-08-23 16:56:18 -0300

    Slides issue when resizing corrected

--- js/main.js
+++ js/main.js
@@ -1,9 +1,37 @@
 $(document).ready(function () {
-  $('.testimonials__container').slick({
+  var $slider = $('.testimonials__container');
+  var timeout;
+
+  $slider.slick({
     dots: true,
-    centerMode: true,
-    centerPadding: '60px',
+    centerMode: false,
     variableWidth: true,
-    slidesToShow: 1
+    slidesToShow: 1,
+    mobileFirst: true,
+    respondTo: 'slider',
+    responsive: [
+      {
+        breakpoint: 992,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '60px',
+          slidesToShow: 1
+        }
+      },
+    ]
+  });
+
+  /**
+   * This fixes the fact that the slide will move after resizing the viewport. (https://github.com/kenwheeler/slick/issues/429)
+   * Usually resizing doesn't happen, but if the user does it he will not the slide has moved towards a side. This centers the slide
+   * 
+   */
+  $(window).resize(function() {
+    if (timeout) clearTimeout(timeout);
+    timeout = setTimeout(function() {
+      $slider.slick('slickGoTo', 0);
+    }, 50);
   });
 });
+
