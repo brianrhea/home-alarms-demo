bb0a9e0472201cd08129e594ac0172d3136108f3
Brian Rhea <brianrhea@gmail.com>
2018-09-04 22:06:20 -0600

    final updates

--- js/main.js
+++ js/main.js
@@ -8,38 +8,40 @@ $(document).ready(function () {
     }
   });
 
-  var $testimonails__slider = $('.testimonials__container');
+  var $testimonials__slider = $('.testimonials__container');
   var timeout;
 
-  $testimonails__slider.slick({
-    dots: true,
-    arrows: true,
-    centerMode: false,
-    variableWidth: true,
-    slidesToShow: 1,
-    mobileFirst: true,  // We approach the configuration as it was for the smallest screen
-    respondTo: 'slider',
-    appendArrows: ".testimonials__nav",
-    appendDots: ".testimonials__nav",
-    responsive: [
-      { // Incrementally apply different configurations for other higher screen sizes
-        breakpoint: 992,
-        settings: {
-          arrows: false,
-          centerMode: true,
-          centerPadding: '60px',
-          slidesToShow: 1
-        }
-      },
-    ]
-  });
+  if ( $testimonials__slider.length ) {
+    $testimonials__slider.slick({
+      dots: true,
+      arrows: true,
+      centerMode: false,
+      variableWidth: true,
+      slidesToShow: 1,
+      mobileFirst: true,  // We approach the configuration as it was for the smallest screen
+      respondTo: 'slider',
+      appendArrows: ".testimonials__nav",
+      appendDots: ".testimonials__nav",
+      responsive: [
+        { // Incrementally apply different configurations for other higher screen sizes
+          breakpoint: 992,
+          settings: {
+            arrows: false,
+            centerMode: true,
+            centerPadding: '60px',
+            slidesToShow: 1
+          }
+        },
+      ]
+    });
+  }
 
   $('.testimonials__nav .slick__prev').on('click', function(){
-    $testimonails__slider.slick('slickPrev');
+    $testimonials__slider.slick('slickPrev');
   });
 
   $('.testimonials__nav .slick__next').on('click', function(){
-    $testimonails__slider.slick('slickNext');
+    $testimonials__slider.slick('slickNext');
   });
 
   /**
@@ -50,50 +52,53 @@ $(document).ready(function () {
   $(window).resize(function() {
     if (timeout) clearTimeout(timeout);
     timeout = setTimeout(function() {
-      $testimonails__slider.slick('slickGoTo', 0);
+      $testimonials__slider.slick('slickGoTo', 0);
     }, 50);
   });
 
   $('.researchers').on('beforeChange', function(event, slick, currentSlide, nextSlide){
     if ( nextSlide === 0 ) {
-      $(".map__map").attr('src', "/img/map__us-mexico.svg").css({'top': '-280px', 'right': '-1290px', 'transform': 'scale(2.5) rotate(-19deg)'});
+      $(".map__map").attr('src', "/img/map__us-mexico.svg").removeClass().addClass('map__map map__us-mexico');
     }
     if ( nextSlide === 3 ) {
-      $(".map__map").attr('src', "/img/map__eastern-europe.svg").css({'top': '470px', 'right': '390px', 'transform': 'scale(5) rotate(26deg)'});
+      $(".map__map").attr('src', "/img/map__eastern-europe.svg").removeClass().addClass('map__map map__eastern-europe');
     }
     if ( nextSlide === 6 ) {
-      $(".map__map").attr('src', "/img/map__us-mexico.svg").css({'top': '-280px', 'right': '-1290px', 'transform': 'scale(2.5) rotate(-19deg)'});
+      $(".map__map").attr('src', "/img/map__us-mexico.svg").removeClass().addClass('map__map map__us-mexico');
     }
     if ( nextSlide === 9 ) {
-      $(".map__map").attr('src', "/img/map__western-europe.svg").css({'top': '300px', 'right': '-50px', 'transform': 'scale(3.5)'});
+      $(".map__map").attr('src', "/img/map__western-europe.svg").removeClass().addClass('map__map map__western-europe');
     }
     if ( nextSlide === 12 ) {
-      $(".map__map").attr('src', "/img/map__british.svg").css({'top': '720px', 'right': '-320px', 'transform': 'scale(5)'});
+      $(".map__map").attr('src', "/img/map__british.svg").removeClass().addClass('map__map map__british');
     }
     if ( nextSlide === 15 ) {
-      $(".map__map").attr('src', "/img/map__canada.svg").css({'top': '330px', 'right': '-800px', 'transform': 'scale(2)'});
+      $(".map__map").attr('src', "/img/map__canada.svg").removeClass().addClass('map__map map__canada');
     }
   });
 
   var $researchers__slider = $('.researchers');
 
-  $researchers__slider.slick({
-    dots: true,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    mobileFirst: true,  // We approach the configuration as it was for the smallest screen
-    appendArrows: ".researchers__nav",
-    appendDots: ".researchers__nav",
-    responsive: [
-      { // Incrementally apply different configurations for other higher screen sizes
-        breakpoint: 992,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 3
-        }
-      },
-    ]
-  });
+  if ( $researchers__slider.length ) {
+    $researchers__slider.slick({
+      dots: false,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      mobileFirst: true,  // We approach the configuration as it was for the smallest screen
+      appendArrows: ".researchers__nav",
+      appendDots: ".researchers__nav",
+      responsive: [
+        { // Incrementally apply different configurations for other higher screen sizes
+          breakpoint: 992,
+          settings: {
+            dots: true,
+            slidesToShow: 3,
+            slidesToScroll: 3
+          }
+        },
+      ]
+    });
+  }
 
   $('.researchers__nav .slick__prev').on('click', function(){
     $researchers__slider.slick('slickPrev');
@@ -101,6 +106,11 @@ $(document).ready(function () {
 
   $('.researchers__nav .slick__next').on('click', function(){
     $researchers__slider.slick('slickNext');
+  });
+
+  $('#toggle-mobile-menu').on('click', function(){
+    $('#mobile-menu').toggleClass('in');
+    $('.menu__image').toggleClass('in');
   });
 
 });
